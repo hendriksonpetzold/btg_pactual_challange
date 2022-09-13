f9bc7cb8a9a57e9c2497c5766245c17f6d2b4fc7
Hendrikson Petzold <hendrikson_14@hotmail.com>
2022-09-13 14:26:46 -0300

    fix:unused imports

--- lib/config/routes.dart
+++ lib/config/routes.dart
@@ -1,8 +1,6 @@
-import 'package:btg_pactual_challange/external/movie_datasource_impl.dart';
 import 'package:btg_pactual_challange/presenter/pages/home/home_controller.dart';
 import 'package:btg_pactual_challange/presenter/pages/home/home_page.dart';
-import 'package:dio/dio.dart';
-import 'package:flutter/material.dart';
+
 import 'package:get/get.dart';
 
 class Routes {
